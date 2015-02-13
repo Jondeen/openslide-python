3c9fc717cd7de9743f93462efac1b674fa20c06a
Jonas Daniel <jonas@ogaard.no>
2015-02-13 08:26:14 +0100

    Init

--- examples/deepzoom/static/z9-t.js
+++ examples/deepzoom/static/z9-t.js
@@ -3,6 +3,29 @@ var lastMethod = "RGB";
 var threshTimer;
 var viewer;
 
+function Channel(n) {
+	var self = this;
+	self.id = n;
+	self.minVal = ko.observable('0');
+	self.maxVal = ko.observable('255');
+	self.inverted = ko.observable(false);
+	self.values = ko.computed({
+		read: function() {
+			return [this.minVal(),this.maxVal()];
+		},
+		write: function(newValue) {
+			this.minVal(newValue[0]);
+			this.maxVal(newValue[1]);
+		},
+		owner: this
+	});
+	self.checked=ko.observable(false);
+	return self;
+};
+
+channels = [];
+for (i=0; i < 3; i++) channels[i] = (new Channel(i));
+
 function bufferedRefreshTiles(viewer) {
     if (threshTimer) {
         clearTimeout(threshTimer);
@@ -52,17 +75,9 @@ function refreshTiles(viewer) {
     viewer.drawer.update();
 }
 
+
+
 $(document).ready(function() {
-    { // Threshold UI
-        var a = $(".thresholdselector");
-        a[0].id = "channel1";
-        var b = a.clone();
-        b[0].id = "channel2";
-        b.appendTo(a.parent());
-        var c = b.clone();
-        c[0].id = "channel3";
-        c.appendTo(a.parent());
-    }
 
     viewer = new OpenSeadragon({
         id: "view",
@@ -90,9 +105,6 @@ $(document).ready(function() {
     viewer.addHandler("open", function() {
         originalSource = viewer.source.tilesUrl;
         $("#name .filename")[0].innerHTML = originalSource;
-        $("#macroFile1")[0].href = originalSource + "semi-macro/7000_7000.jpeg";
-        $("#macroFile2")[0].href = originalSource + "semi-macro/5000_5000.jpeg";
-        $("#macroFile3")[0].href = originalSource + "semi-macro/3000_3000.jpeg";
         // Loadtime efficency Hack
         viewer.source.minLevel = 8;
     });
@@ -125,86 +137,20 @@ $(document).ready(function() {
     });
 	
     $("#sett_butt").on("change", function() {
-        refreshTiles(viewer);
+        //refreshTiles(viewer);
     });
 
     $("#matrix").on("change", function() {
-        viewer.debugMode = $("#matrix")[0].checked;
-        refreshTiles(viewer);
+        //viewer.debugMode = $("#matrix")[0].checked;
+        //refreshTiles(viewer);
     });
 	
     $(".inverting input").on("change", function() {
         if ($("#sett_butt")[0].checked) {
-            bufferedRefreshTiles(viewer);
+            //bufferedRefreshTiles(viewer);
         }
     });
-    $("#change_input .channel-slider").slider({
-        range: true,
-        min: 0,
-        max: 255,
-        values: [0, 255],
-        change: function(event, ui) {
-            o = $(this).parent()
-            o.find("span.channel-value")[0].innerHTML = ui.values[0] + "-" + ui.values[1];
-            if ($("#sett_butt")[0].checked) {
-                bufferedRefreshTiles(viewer);
-            }
-        },
-        slide: function(event, ui) {
-            o = $(this).parent();
-            o.find("span.channel-value")[0].innerHTML = ui.values[0] + "-" + ui.values[1];
-        }
-    });
+    
 
-
-
-	for (i=0;i<2;i++) {
-		for (j=0;j<2;j++) {
-			theEnd=[".low",".high"][i];
-			theDirection=[".less",".more"][j];
-			$(theEnd+theDirection).on("click", function() {
-				e=$(this).hasClass("high");
-				d=$(this).hasClass("more");
-				slider = $(this).parent().find(".channel-slider");
-				values = slider.slider("option", "values");
-				delta=(d?1:-1);
-				values=[values[0]+(e?0:delta),values[1]+(e?delta:0)];
-				slider.slider("option", "values", values);
-			});
-		}
-	}			
-	
-    $("#change_input .ui-slider-range").css({
-        opacity: 0.75
-    });
-    $("#method").on("change", function() {
-        refreshTiles(viewer);
-        $("#change_input .channel-slider").removeClass(lastMethod)
-        $("#change_input .channel-slider").addClass($("#method")[0].selectedOptions[0].innerHTML)
-        lastMethod = $("#method")[0].selectedOptions[0].innerHTML;
-    });
-    //$("#upd_butt").button();
-    $("#upd_butt").on("click", function() {
-        refreshTiles(viewer);
-    });
-    //$("#change_input option,select,input,td,label,span,button").css('font-size', '12px');
-    if (location.hash.length > 1) {
-        hash = location.hash.substring(1).split("=");
-        method = hash[0];
-		$("#method").val(method);
-        vals = hash[1].split("&");
-        for (i = 0; i < 3; i++) {
-            slider = $($(".channel-slider")[i]);
-            val = vals[i].split(":");
-            val = [Number(val[0]), Number(val[1])];
-            if (val[0] < 0 || val[1] < 0) {
-                val[0] *= -1;
-                val[1] *= -1;
-                $(".inverting input")[i].checked = true;
-            }
-            slider.slider("option", "values", val);
-        }
-        $("#sett_butt")[0].checked = true;
-        bufferedRefreshTiles(viewer);
-    }
+	ko.applyBindings(channels);
 });
